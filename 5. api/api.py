c8e607315571e50b00c971ba3a11117dbf096954
Hasibur013 <hasibur01631753881@gmail.com>
2024-07-02 10:08:52 +0600

    api

---
+++ 5. api/api.py
@@ -0,0 +1,11 @@
+# A Python module for network requets
+import requests
+
+# API URL
+url = 'https://dummyjson.com/products'
+
+# Get data from API
+response = requests.get(url)
+
+# Show data to screen
+print(response.text)
